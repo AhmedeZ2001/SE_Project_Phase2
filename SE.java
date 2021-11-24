684394b846f1b7ab13536fe1e15adbb0bb4d308f
abdallahFc <84878301+abdallahFc@users.noreply.github.com>
2021-11-24 17:54:08 +0200

    Update SE.java

--- SE.java
+++ SE.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ 20190327-20190589-20190047-20190771
  */
 package se;
 
